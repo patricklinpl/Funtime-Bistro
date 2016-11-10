bd219b8e01c933ad1acf7d455a8e77af6fe0289f
Patrick Lin <patricklin@alumni.ubc.ca>
2016-11-09 19:03:32 -0800

    updated insert to support auto_inc, changes to menuitem/ing, and added more tuples for tables

--- proj-docs/proj_insert.sql
+++ proj-docs/proj_insert.sql
@@ -1,104 +1,100 @@
-insert into Users values
-('omgitzme', 'admin', 'hihi', 'Harry Zhi', '6042312412', 
-'6223 Bateman St.', DATE '1961-01-01', 'F');
+insert into Users values ('omgitzme', 'admin', 'hihi', 'Harry Zhi', '6042312412', '6223 Bateman St.', DATE '1961-01-01', 'F');
 
-insert into Users values
-('Wewic', 'admin', 'password', 'Eric Wu', '6041232040', 
-'604 Vancouver Ave.', DATE '1995-04-18', 'F');
+insert into Users values ('Wewic', 'chef', 'password', 'Eric Wu', '6041232040', '604 Vancouver Ave.', DATE '1995-04-18', 'F');
 
-insert into Users values
-('ptlin', 'admin', 'greetings', 'Patrick Lin', '6041235235', 
-'6224 Bateman St.', DATE '1961-01-01', 'F');
+insert into Users values ('ptlin', 'customer', 'greetings', 'Patrick Lin', '6041235235', '6224 Bateman St.', DATE '1961-01-01', 'F');
 
-insert into Users values
-('quanbao', 'admin', 'ptran', 'Paul Tran', '6041252341', 
-'6225 Bateman St.', DATE '1961-01-02', 'F');
+insert into Users values ('quanbao', 'customer', 'ptran', 'Paul Tran', '6041252341', '6225 Bateman St.', DATE '1961-01-02', 'F');
 
-insert into Users values
-('newuser', 'customer', 'random', 'John Smith', '8329103291', 
-'1513 Las Vegas St.', DATE '2016-11-01', 'F');
+insert into Users values ('newuser', 'customer', 'random', 'John Smith', '8329103291', '1513 Las Vegas St.', DATE '2016-11-01', 'F');
 
-insert into Users values
-('chef1', 'chef', 'cooking', 'Rordon Gamsey', '8329192919', 
-'1514 Las Vegas St.', DATE '2016-11-02', 'F');
-
-insert into Chef values
-('chef1', 'omgitzme', 100000, 234213205);
-
-insert into Orders values
-(1, 'newuser', 'chef1', DATE '2016-11-01', 
-NULL, 'processing');
-
-insert into Orders values
-(2, 'newuser', 'chef1', DATE '2016-11-02', 
-NULL, 'processing');
-
-insert into Invoice values
-(1, 'newuser', 34.50, DATE '2016-11-01', 'credit');
-
-insert into Invoice values
-(2, 'newuser', 125.23, DATE '2016-11-02', 'cash');
-
-insert into Menuitem values
-('Shrimp Fried Rice', 11.00, 'http://', 'Egg fried rice with shrimps', 'F');
-
-insert into Menuitem values
-('Deluxe Burger', 8.95, 'http://', 'Hamburger with lettuce, tomato, pickles, and 
-	a beef patty', 'F');
-
-insert into Ingredient values
-('Lettuce', 8, 'F');
-
-insert into Ingredient values
-('Tomato', 6, 'F');
-
-insert into Ingredient values
-('Burger Buns', 4, 'F');
-
-insert into Ingredient values
-('Beef Patty', 4, 'F');
-
-insert into Ingredient values
-('Rice', 5, 'F');
-
-insert into Ingredient values
-('Egg', 4, 'F');
-
-insert into Ingredient values
-('Shrimp', 4, 'F');
-
-insert into Contains values
-(1, 'Shrimp Fried Rice', 1);
-
-insert into Contains values
-(1, 'Deluxe Burger', 1);
-
-insert into Contains values
-(2, 'Shrimp Fried Rice', 3);
-
-insert into MadeOf values
-('Shrimp Fried Rice', 'Rice');
-
-insert into MadeOf values
-('Shrimp Fried Rice', 'Egg');
-
-insert into MadeOf values
-('Shrimp Fried Rice', 'Shrimp');
-
-insert into MadeOf values
-('Deluxe Burger', 'Burger Buns');
-
-insert into MadeOf values
-('Deluxe Burger', 'Lettuce');
-
-insert into MadeOf values
-('Deluxe Burger', 'Tomato');
-
-insert into MadeOf values
-('Deluxe Burger', 'Beef Patty');
+insert into Users values ('chef1', 'chef', 'cooking', 'Rordon Gamsey', '8329192919', '1514 Las Vegas St.', DATE '2016-11-02', 'F');
 
 
+insert into Chef values ('chef1', 'omgitzme', 100000, 234213205);
+
+insert into Chef values ('Wewic', 'omgitzme', 150405, 234013005);
 
 
+insert into Orders (customer_userName, chef_userName, orderdate, cookeddate, status) values ('newuser', 'chef1', DATE '2016-11-01', DATE '2016-11-01', 'open');
+
+insert into Orders (customer_userName, chef_userName, orderdate, cookeddate, status) values ('ptlin', 'Wewic', DATE '2016-11-02', DATE '2016-11-01', 'open');
+
+insert into Orders (customer_userName, chef_userName, orderdate, cookeddate, status) values ('quanbao', 'Wewic', DATE '2016-11-01', DATE '2016-11-01', 'open');
 
 
+insert into Invoice values (1, 'newuser', 34.50, DATE '2016-11-01', 'credit');
+
+insert into Invoice values (2, 'ptlin', 125.23, DATE '2016-11-02', 'cash');
+
+insert into Invoice values (3, 'quanbao', 125.23, DATE '2016-11-02', 'cash');
+
+
+insert into Menuitem values ('Shrimp Fried Rice', 11.00, 'http://', 'Egg fried rice with shrimps', 3, 'F');
+
+insert into Menuitem values ('Deluxe Burger', 8.95, 'http://', 'Hamburger with lettuce, tomato, pickles, and a beef patty', 5, 'F');
+
+insert into Menuitem values ('Ice cream ramen', 5.23, 'http://', 'Noodles in a cone!', 10, 'F');
+
+insert into Menuitem values ('Beef smoothie', 6.21, 'http://', 'A healthy blend of greens and beef to ensure you hit your protein macros.', 1, 'F');
+
+insert into Menuitem values ('Deep fried protein powder', 18.43, 'http://', 'Anything is better deep fried!', 15, 'F');
+
+
+insert into Ingredient values ('Lettuce', 'F');
+
+insert into Ingredient values ('Tomato',  'F');
+
+insert into Ingredient values ('Burger Buns', 'F');
+
+insert into Ingredient values ('Beef Patty', 'F');
+
+insert into Ingredient values ('Rice',  'F');
+
+insert into Ingredient values ('Egg',  'F');
+
+insert into Ingredient values ('Shrimp', 'F');
+
+insert into Ingredient values ('Ramen', 'F');
+
+insert into Ingredient values ('Ice cream', 'F');
+
+insert into Ingredient values ('Protein', 'F');
+
+
+insert into Contains values (1, 'Shrimp Fried Rice', 2);
+
+insert into Contains values (1, 'Deluxe Burger', 3);
+
+insert into Contains values (2, 'Ice cream ramen', 3);
+
+insert into Contains values (2, 'Beef smoothie', 1);
+
+insert into Contains values (2, 'Shrimp Fried Rice', 1);
+
+insert into Contains values (3, 'Deep fried protein powder', 10);
+
+insert into Contains values (3, 'Deluxe Burger', 2);
+
+
+insert into MadeOf values ('Shrimp Fried Rice', 'Rice');
+
+insert into MadeOf values ('Shrimp Fried Rice', 'Egg');
+
+insert into MadeOf values ('Shrimp Fried Rice', 'Shrimp');
+
+insert into MadeOf values ('Deluxe Burger', 'Burger Buns');
+
+insert into MadeOf values ('Deluxe Burger', 'Lettuce');
+
+insert into MadeOf values ('Deluxe Burger', 'Tomato');
+
+insert into MadeOf values ('Deluxe Burger', 'Beef Patty');
+
+insert into MadeOf values ('Ice cream ramen', 'Ramen');
+
+insert into MadeOf values ('Ice cream ramen', 'Ice cream');
+
+insert into MadeOf values ('Beef smoothie', 'Beef Patty');
+
+insert into MadeOf values ('Deep fried protein powder', 'Protein');
